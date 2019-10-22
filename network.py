97bfb2f36fae41864519a9b43c1fe9122794994c
Tony <otano0398@gmail.com>
2019-10-22 10:54:41 -0400

    Code refactored

--- network.py
+++ network.py
@@ -19,6 +19,8 @@ class Peer:
         key = utils.generate_random_id()
         msg['key'] = key
         msg['sender_id'] = self.node.ID
+
+
         self.socket.sendto(json.dumps(msg).encode(), addr)
 
         start = datetime.datetime.now()
@@ -35,35 +37,91 @@ class Peer:
         # raise Exception("The remote peer is not responding")
 
     def serve(self):
-        while True:
-            msg, addr = self.socket.recvfrom(1024)
-            data = json.loads(msg)
+        print('Peer with ID ' + str(self.node.ID) + " serving on port: " + str(self.node.port))
 
-            if 'result' in data:
+        #If the peer doesn't receive a response after five seconds to it's request it desists of waiting for it
+        self.socket.settimeout(5)
+
+        #A list of requests the peer has sent but doesn't have received a response yet.
+        #Every element has the structure key, address()
+        pending_answers = []
+
+        while True:
+            try:
+                msg, addr = self.socket.recvfrom(1024)
+            except:
+                #If a TimeOut Error ocurs, the result of all pending requests is None
+                for pa in pending_answers:
+                    key, addr = pa
+                    answer = {'operation': 'RESPONSE', 'result': None, 'key': key}
+                    self.socket.sendto(json.dumps(answer).encode(), addr)
+                pending_answers.clear()
+
                 continue
 
-            result = None
 
-            print('ALERT')
-            print(data)
-            print('ALERT')
+            data = json.loads(msg)
+            print("Data received: " + str(data))
 
-            if data['method'] == 'FIND_NODE':
-                result =  self.node.FIND_NODE(data['id'])
-            elif data['method'] == 'FIND_VALUE':
-                result = self.node.FIND_VALUE(data['id'])
-            elif data['method'] == 'PING':
-                result = self.node.PING()
-            elif data['method'] == 'STORE':
-                result = self.node.STORE(data['storeKey'], data['storeValue'])
+            # A peer has to perform a method specified by other peer by RPC
+            if data['operation'] == 'EXECUTE':
+                result = None
+                if data['method'] == 'FIND_NODE':
+                    result =  self.node.FIND_NODE(data['id'])
+                elif data['method'] == 'FIND_VALUE':
+                    result = self.node.FIND_VALUE(data['id'])
+                elif data['method'] == 'PING':
+                    result = self.node.PING()
+                elif data['method'] == 'STORE':
+                    result = self.node.STORE(data['store_key'], data['storeValue'])
 
-            if result is not None:
-                answer = {'result': result, 'key': data['key']}
-                self.socket.sendto(json.dumps(answer).encode(), addr)
+                if result is not None:
+                    answer = {'operation': 'RESPONSE', 'result': result,
+                             'key': data['key'], 'sender_id': self.node.ID }
+                    answer = json.dumps(answer).encode()
+                    self.socket.sendto(answer, addr)
+                    self.update(data['sender_id'])
+
+            # A peer is requested to perform a RPC to other peer
+            if data['operation'] == 'RPC':
+                msg = None
+
+                if data['method'] == 'FIND_NODE':
+                    msg =  utils.build_FIND_NODE_msg(data['id'], self.node.ID)
+                elif data['method'] == 'FIND_VALUE':
+                    msg =  utils.build_FIND_VALUE_msg(data['id'], self.node.ID)
+                elif data['method'] == 'PING':
+                    msg =  utils.build_PING_msg(self.node.ID)
+                elif data['method'] == 'STORE':
+                    msg = utils.build_STORE_msg(data['storeKey'], data['store_value'], self.node.ID)
+
+                # Add the answer of the request to the list of pending answers
+                pending_answers.append((msg['key'], addr))
+
+                if msg is not None:
+                    # The address of the remote peer wich it will be used as the target of the RPC
+                    addr = (data['ip'], data['port'])
+
+                    msg = json.dumps(msg).encode()
+                    print("Attempting to send data to: " + str(addr))
+                    print("Data to send: " + str(msg))
+
+                    self.socket.sendto(msg, addr)
+                    print("RPC done to: " + str(addr))
+
+            # The peer receives the answer of a RPC made before
+            if data['operation'] == 'RESPONSE':
+                #Look for the key of the request in the pending answers
+                for pa in pending_answers:
+                    if pa[0] == data['key']:
+                        #remove it
+                        pending_answers.remove(pa)
+                        #send the answer to the client
+                        self.socket.sendto(json.dumps(data).encode(), pa[1])
                 self.update(data['sender_id'])
 
-
     def update(self, senderID):
+        print("Updating" + str(self.node) + "...")
         # senderID, senderIp, senderPort = node
         kBucket = self.node.find_kBucket(senderID)
 
@@ -76,7 +134,7 @@ class Peer:
             else:
                 # last_recently_seen node
                 _, lastNodeIp, lastNodePort = kBucket[0]
-                msg = utils.build_PING_msg()
+                msg = utils.build_PING_msg(self.node.ID)
 
                 try:
                     self.RPC(msg, (lastNodeIp, lastNodePort))
@@ -85,6 +143,7 @@ class Peer:
                 except:
                     kBucket.remove(kBucket[0])
                     kBucket.append(senderID)
+        self.node.print_routing_table()
 
     def lookup(self, ID):
         #Insert all nodes of the k-bucket list of start on a list
@@ -106,7 +165,7 @@ class Peer:
 
                 # k_closest = n[1].FIND_NODE(ID, self.node.k)
                 k_closest = None
-                msg = utils.build_FIND_NODE_msg(ID)
+                msg = utils.build_FIND_NODE_msg(ID, self.node.ID)
                 try:
                     k_closest = self.RPC(msg, (n[1][0], n[1][2]))
                 except:
@@ -143,51 +202,15 @@ class Peer:
         return heapq.nsmallest(self.node.k, enquired)
 
 
-def serve(peer):
-    peer.serve()
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=8000)
+    parser.add_argument('-i', '--id', type=int, default=1)
 
-def start():
-    ID = 1
-    node = Node(ID, '127.0.0.1', 8000, B=3)
+    args = parser.parse_args()
+
+    ID = args.id
+    node = Node(ID, '127.0.0.1', args.port, B=3, k=2)
     peer = Peer(node)
-
-    import threading
-    threading._start_new_thread(serve, (peer, ))
-
-    while True:
-        print('Peer raised at localhost:8000')
-        x = input('Write input for testing\n').split(' ')
-
-        if x[0] == 'PING':
-            x[1] = int(x[1])
-            ans = peer.RPC(utils.build_PING_msg(), ('localhost', x[1]))
-            print(ans)
-
-start()
-
-# if __name__ == '__main__':
-#     import argparse
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('-p', '--port', type=int, default=8000)
-#     parser.add_argument('-i', '--id', type=int, default=1)
-
-#     args = parser.parse_args()
-
-#     ID = args.id
-#     node = Node(ID, '127.0.0.1', args.port, B=3)
-#     peer = Peer(node)
-
-#     import threading
-
-#     threading._start_new_thread(serve, (peer, ))
-
-#     # while True:
-#     actions = input('Write [RPC func] [args separated by spaces]\n').split(' ')
-
-#     if actions[0] == 'PING':
-#         port = int(actions[1])
-
-#         msg = utils.build_PING_msg()
-#         answer = peer.RPC(msg, ('localhost', port))
-#         print('LLEGUE AKI')
-#         print(answer)
+    peer.serve()
--- testing.py
+++ testing.py
@@ -1,37 +1,49 @@
-def generate(address_space):
-    nodes = []
-    c = 2 ** address_space
-    for i in range(c):
-        n = Node(i, '127.0.0.1', 8080)
-        n.route_table = [[] for j in range(address_space)]
-        nodes.append(n)
+import socket
+import json
 
 
-    for current_node in nodes:
-        for target_node in nodes:
 
-            distance = current_node.ID ^ target_node.ID
-            # print('current node: ',current_node)
-            # print('target_node:   ', target_node)
-            # print('distance: ', distance)
+if __name__ == "__main__":
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('localhost', 5000))
+
+    s.settimeout(6)
+
+    import io
+
+    reader = open('input', 'r')
+    commands = reader.readlines()
+
+    # while True:
+    for command in commands:
+        input_msg = command.split()
+        # input_msg = input_msg[0:-1] if input_msg[-1] == '\n' else input_msg
+        # input_msg = input("[PORT] [OPERATION] [OP_PORT] [METHOD] [ARGS...]\n").split()
+
+        addr = 'localhost', int(input_msg[0])
+        data = {'operation': input_msg[1], 'method': input_msg[3], 'ip': 'localhost', 'port': int(input_msg[2])}
+        for arg in input_msg[4:]:
+            arg_name, arg_value = tuple(arg.split(':'))
+
+            if arg_name in ['port', 'ID']:
+                data[arg_value] = int(arg_value)
+            else: data[arg_name] = arg_value
+
+        print("data to send to " + str(addr[0])+ ":" + str(addr[1]) + "\n" + str(data))
+        s.sendto(json.dumps(data).encode(), addr)
+
+        print("Waiting for an answer from " + str(addr))
+        try:
+            data, addr1 = s.recvfrom(1024)
+            data = json.loads(data)
+        except:
+            print("Timeout error")
+            continue
 
 
-            if distance:
-                rt_pos = int(math.log2(distance))
-                # print('rt_pos: ', rt_pos)
-                current_node.route_table[rt_pos].append(target_node)
-
-            print('\n')
-
-    return nodes
+        print("Answer from RPC executded from address " + str(addr[0]) + ":" + str(addr[1]) + "is " + str(data['result']))
+    
+    reader.close()
 
 
-def xor_table(address_space):
-    t = []
-    for i in range(address_space):
-        t.append([])
-        for j in range(address_space):
-            t[i].append(i ^ j)
-
-    for r in t:
-        print(r)
