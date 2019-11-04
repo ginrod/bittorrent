280070a4a7ae27d832e785002789d8b6e3000b4e
Tony <otano0398@gmail.com>
2019-11-03 22:53:53 -0500

    Added function 'generate_pieces'

--- metainfo.py
+++ metainfo.py
@@ -6,15 +6,11 @@ def generate_hash(piece, hash_func):
     h = hash_func(piece.encode())
     return int.from_bytes(h.digest(), byteorder = sys.byteorder)
 
-
-
-# def create_metainfo(files, mode, piece_length=256*1024):
-
-
-#     metainfo = {
-#         "info": {
-#             "piece length": piece_length
-
-#         }
-#     }
-
+def generate_pieces(_file, piece_length):
+    pieces = []
+    while True:
+        piece = _file.read(piece_length)
+        if not piece:
+            break
+        pieces.append(generate_hash(piece, hashlib.sha1))
+    return pieces
