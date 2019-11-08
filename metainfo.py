3fd367f71e1a7b456a89d810d4b2d5e6b7cb57c0
Tony <otano0398@gmail.com>
2019-11-08 14:57:39 -0500

    Creation and parsing of .torrent

--- metainfo.py
+++ metainfo.py
@@ -4,6 +4,8 @@ import os
 import torrent_parser
 import time
 
+
+
 def generate_hash(piece, hash_func):
     h = hash_func(piece.encode())
     return h.hexdigest()
@@ -19,17 +21,11 @@ def generate_pieces(paths, piece_length):
                 pieces.append(generate_hash(piece, hashlib.sha1))
     return pieces
 
-def concat_pieces(pieces):
-    result = ""
-    for piece in pieces:
-        result += piece
-    return result
-
 def create_metainfo(paths, announce, announce_list=[], mode="single-file", piece_length=256*1024, encoding="utf-8", comment="", created_by=""):
     metainfo = {
         "info": {
             "piece_length": piece_length,
-            "pieces": concat_pieces(generate_pieces(paths, piece_length))
+            "pieces": generate_pieces(paths, piece_length)
         },
 
         "announce": announce,
@@ -53,26 +49,13 @@ def create_metainfo(paths, announce, announce_list=[], mode="single-file", piece
         metainfo["info"]["files"] = files
 
     else: raise Exception("Invalid file mode")
+    print(metainfo['info']['pieces'])
+    return metainfo
 
-    return (metainfo["info"]["name"], torrent_parser.encode(metainfo))
-
-def create_torrent(name_and_metainfo):
-    name, bencoded_metainfo = name_and_metainfo
-    with open(name + ".torrent", "w") as torrent:
-        torrent.write(str(bencoded_metainfo))
+def create_torrent(metainfo_dict):
+    name = metainfo_dict["info"]["name"] + ".torrent"
+    torrent_parser.create_torrent_file(name, metainfo_dict)
     return name
 
-# def decode_torrent(torrent_name):
-#     metainfo = None
-#     with open(torrent_name) as torrent:
-#         data_bencoded = ""
-#         r = torrent.read(1024)
-#         while r:
-#             data_bencoded += r
-#             r = torrent.read(1024)
-#         metainfo = torrent_parser.decode(data_bencoded, encoding="utf-8")
-#     return metainfo
-    
-# name, metainfo = create_metainfo(["metainfo.py"], "tracker.com", piece_length=100, comment="This is a comment", created_by="BitTorrentMatcom")
-# dotTorrent = create_torrent((name, metainfo))
-# print(decode_torrent("metainfo.py.torrent"))
+def decode_torrent(torrent_name):
+    return torrent_parser.parse_torrent_file(torrent_name)
