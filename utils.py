1cfd94d96a29aeb0e8bba8eafe8288089579449d
ginrod <g.gonzalez%40estudiantes.matcom.uh.cu>
2019-12-01 00:12:42 +0100

    utils merged

--- utils.py
+++ utils.py
@@ -1,10 +1,47 @@
 import uuid
 import socket
 import time
-import json, datetime
+import json, datetime, os
 
 import torrent_parser
 import hashlib
+
+# Kademlia ID used to do a name index in database
+INDEX_KEY = 10
+
+def get_prefixes(name):
+    prefixes = set()
+    for i in range(len(name) + 1):
+        pref = name[0:i]
+        prefixes.add(pref)
+    
+    return prefixes
+
+def get_suffixes(name):
+    suffixes = set(['', name])
+    for i in range(len(name) + 1):
+        suff = name[i:len(name)]
+        suffixes.add(suff)
+    
+    return suffixes
+
+def get_prefixes_and_suffixes(name):
+    substrings = set(['', name])
+    for i in range(len(name) + 1):
+        pref = name[0:i]
+        suff = name[i:len(name)]
+        substrings.add(pref)
+        substrings.add(suff)
+
+    return substrings
+
+def get_substrings(name):
+    substrings = set(['', name])
+    for i in range(len(name)):
+        for j in range(i + 1, len(name) + 1):
+            substrings.add(name[i:j])
+    
+    return substrings
 
 def parse_to_json(obj):
     if isinstance(obj, datetime.datetime):
@@ -38,6 +75,8 @@ def dump_json(data, path):
     with open(path, 'w') as json_file:
         json.dump(data, json_file, default=parse_to_json)
 
+def dumps_json(data):
+    return json.dumps(data, default=parse_to_json)
 
 
 def build_PING_msg(sender):
@@ -77,23 +116,28 @@ def load(name):
     return f
 
 
-# def sendall_to(msg, addr, socket, time_out=5):
-#     sent = 0
-#     start = time.time()
-#     while sent < len(msg) and time.time() - start < time_out:
-#         sent += socket.sendto(msg, addr)
-#     if sent != len(msg):
-#         raise socket.timeout
+def save_file(path, file_bytes):
+    with open(path, 'wb') as f:
+        f.write(file_bytes)
 
-def get_answer(expected_key, s:socket.socket, attempts=3):
+def load_file(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+    
+    return data
 
-    for _ in range(attempts):
-        data = json.loads(s.recvfrom(1024)[0])
+def close_connection(sock):
+    try: sock.shutdown(socket.SHUT_WR)
+    except: pass
+    try: sock.close()
+    except: pass
 
-        if data['key'] == expected_key:
-            return data
+def create_dirs(path):
+    try: os.makedirs(path)
+    except OSError: pass
 
-    raise socket.timeout
+def assign(data, name='', to_update=False):
+    return (data, name, to_update) 
 
 def build_xor_table(address_space):
     xor_table = [[i ^ j for i in range(address_space)] for j in range(address_space)]
